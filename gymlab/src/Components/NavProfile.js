7869680a659442d301a215efaa78b02d3b0e6850
Bartek <batek31221@gmail.com>
2024-05-25 11:37:28 +0200

    Update NavProfile.js

--- gymlab/src/Components/NavProfile.js
+++ gymlab/src/Components/NavProfile.js
@@ -8,7 +8,7 @@ const NavProfile = () => {
   return <div className="NavProfileContainer">
     <AccountCircleIcon className="profileIcon"/>
     <div className="navJoinContainer">
-        <Link to="/membership" className="navJoinButton"><AddIcon className="navJoinButtonIcon"/></Link>
+        <Link to="/GymLab/membership" className="navJoinButton"><AddIcon className="navJoinButtonIcon"/></Link>
         <h4 className="navJoinHeading">JOIN CLASS NOW</h4>
     </div>
   </div>;
